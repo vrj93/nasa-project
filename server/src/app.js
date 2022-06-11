9e389d27c89b6e7e83ef1357ec5ce033a6cd9f99
Vivek Joshi <vivekjoshi@topsinfosolutions.com>
2022-06-11 19:01:07 +0530

    Optimizing front end app, making code deployable into production.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,6 +8,11 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRoute);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
