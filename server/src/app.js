b9c76f237727aa56a7f7d51d19bb1a1456513c2c
Vivek Joshi <vrj022@gmail.com>
2024-02-08 21:41:17 +0530

    API versioning

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,8 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const api = require('./routes/api');
 const app = express();
-const planetsRoute = require('./routes/planets/plantes.route');
-const launchesRoute = require('./routes/launches/launches.route');
 
 app.use(cors({
     origin: process.env.FRONT_ORIGIN,
@@ -14,8 +13,8 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/planets', planetsRoute);
-app.use('/launches', launchesRoute);
+
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
