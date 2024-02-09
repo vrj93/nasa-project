8316041fd18abd658268179e9be61780cd530eba
Vivek Joshi <vrj022@gmail.com>
2024-02-09 15:27:03 +0530

    SpaceX launch data import functionality implemented.

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,7 @@ const app = require('./app');
 
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 require('dotenv').config({path: path.resolve('../.env')});
 const PORT = process.env.PORT || 8000;
@@ -17,6 +18,7 @@ const server = http.createServer(app);
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
