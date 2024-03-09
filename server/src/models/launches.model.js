1fdb5043816c709a738f6ec9153882c64791c06a
Vivek Joshi <vrj022@gmail.com>
2024-03-09 20:16:28 +0530

    Pagination added for Launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -101,8 +101,12 @@ async function getLatestLaunch() {
   }
 }
 
-async function getAllLaunches() {
-  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function scheduleNewLaunch(launch) {
